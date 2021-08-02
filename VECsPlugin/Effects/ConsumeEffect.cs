e2186acca84757f1aa88ecf3191ceb934fdb8be8
Adele Reed <virepri2k@gmail.com>
2021-08-01 21:56:59 -0700

    Fix consume hook

--- VECsPlugin/Effects/ConsumeEffect.cs
+++ VECsPlugin/Effects/ConsumeEffect.cs
@@ -7,24 +7,31 @@ namespace VECsPlugin.Effects
         public const float ConsumeIncreaseDegree = 0.5f; // % of a bullet's damage
 
         private float ConsumeDegree = 0f;
+        private CharacterData data;
 
         public void PrepareOnce(CharacterData data)
         {
-            data.block.BlockProjectileAction += (o, vector3, arg3) =>
-            {
-                UnityEngine.Debug.Log("Blocked projectile");
-                var phit = o.GetComponent<ProjectileHit>();
-                data.healthHandler.Heal(phit.damage * ConsumeDegree);
-
-                UnityEngine.Object.Destroy(o);
-            };
+            this.data = data;
+            data.block.BlockProjectileAction += OnBlockProjectileAction;
         }
+        
+        private void OnBlockProjectileAction(GameObject o, Vector3 fwd, Vector3 pos)
+        {
+            UnityEngine.Debug.Log("Blocked projectile");
+            var phit = o.GetComponent<ProjectileHit>();
+            data.healthHandler.Heal(phit.damage * ConsumeDegree);
+
+            UnityEngine.Object.Destroy(o);
+        } 
         
         public void IncreaseConsumeDegree(float by)
         {
             ConsumeDegree += by;
         }
-        
-        public void EndGame() { }
+
+        public void EndGame()
+        {
+            data.block.BlockProjectileAction -= OnBlockProjectileAction;
+        }
     }
 }
